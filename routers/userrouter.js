4645c12dd8346f5fb7639499053ea147a408e216
‪ahmed adel‬‏ <59086439+Ahmedadel449@users.noreply.github.com>
2023-02-12 01:27:45 +0200

    Update userrouter.js

--- routers/userrouter.js
+++ routers/userrouter.js
@@ -9,28 +9,28 @@ user_route.use(bodyParser.urlencoded({extended:true}));
 const multer = require("multer");
  const path = require("path")
 
-user_route.use(express.static('public'));
-const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,path.join(__dirname,'../public/userimages'),function(error,success){
-            if(error) throw error
-        });
-    },
-    filename:function(req,file,cb){
-        const name = Date.now()+'-'+file.originalname;
-        cb(null,name , function(error1,success1){
-            if (error1) throw error1
-        })
+// user_route.use(express.static('public'));
+// const storage = multer.diskStorage({
+//     destination:function(req,file,cb){
+//         cb(null,path.join(__dirname,'../public/userimages'),function(error,success){
+//             if(error) throw error
+//         });
+//     },
+//     filename:function(req,file,cb){
+//         const name = Date.now()+'-'+file.originalname;
+//         cb(null,name , function(error1,success1){
+//             if (error1) throw error1
+//         })
 
-    }
-})
+//     }
+// })
 
-const upload = multer({storage:storage});
+// const upload = multer({storage:storage});
 
 const user_controller = require("../controllers/usercontroller");
 const auth =require("../middleware/auth");
 
-user_route.post('/register', upload.single('image'),user_controller.register_user)
+user_route.post('/register', user_controller.register_user)
 user_route.post('/login',user_controller.user_login)
 user_route.post('/dola',user_controller.dola)
 user_route.get('/test',auth,function(req,res){
