9bf38629bcbec1e8e28d70f5ddec52b39eb828f5
‪ahmed adel‬‏ <59086439+Ahmedadel449@users.noreply.github.com>
2023-02-16 15:41:17 +0200

    Update userrouter.js

--- routers/userrouter.js
+++ routers/userrouter.js
@@ -9,21 +9,35 @@ user_route.use(bodyParser.urlencoded({extended:true}));
 const multer = require("multer");
  const path = require("path")
 
-user_route.use(express.static('public'));
-const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,path.join(__dirname,'../public/userimages'),function(error,success){
-            if(error) throw error
-        });
-    },
-    filename:function(req,file,cb){
-        const name = Date.now()+'-'+file.originalname;
-        cb(null,name , function(error1,success1){
-            if (error1) throw error1
-        })
+// user_route.use(express.static('public'));
+// const storage = multer.diskStorage({
+//     destination:function(req,file,cb){
+//         cb(null,path.join(__dirname,'../public/userimages'),function(error,success){
+//             if(error) throw error
+//         });
+//     },
+//     filename:function(req,file,cb){
+//         const name = Date.now()+'-'+file.originalname;
+//         cb(null,name , function(error1,success1){
+//             if (error1) throw error1
+//         })
 
-    }
-})
+//     }
+// })
+const storage = multer.diskStorage({
+    destination :function(req,file,callback){
+        callback(null,'./images/')
+    } ,
+
+    filename : function(req,file,callback){
+        callback(null,new Date().toDateString() + file.originalname)
+    } 
+});
+
+const upload = multer({
+    storage:storage,
+
+});
 
 const upload = multer({storage:storage});
 
